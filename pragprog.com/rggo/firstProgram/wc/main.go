b28bbbe529ef844faa70f388fdc733b578ac1c20
Eike Zink <user@eike-zink.de>
2026-01-23 08:18:49 +0100

    Adding Command-Line Flags

--- pragprog.com/rggo/firstProgram/wc/main.go
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	// Define a boolean flag -l to count line instead of words
+	countLines := flag.Bool("l", false, "Count liness")
+	flag.Parse()
+
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *countLines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// Define the scanner split type to words or lines (default)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Define a counter
 	wc := 0
--- pragprog.com/rggo/firstProgram/wc/main_test.go
+++ pragprog.com/rggo/firstProgram/wc/main_test.go
@@ -11,7 +11,19 @@ func TestCountWords(t *testing.T) {
 
 	exp := 3
 
-	res := count(b)
+	res := count(b, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("line1\n line2\n line3\n")
+
+	exp := 3
+
+	res := count(b, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
